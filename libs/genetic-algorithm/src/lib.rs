0c0ecf07d272f096ae6633a961af0b96c1f3ebf5
Niklas <niklaskersten02@gmail.com>
2026-01-19 18:08:09 +0100

    implemented the basic stuff for the nn and the animals (reproduction is still needed)

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,14 +1,122 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+/*
+Contains the implementation of Animals (Prey and Predator) with their neural networks, movement, and mutation logic.
+*/
+
+
+use rand::RngCore;
+use rand::Rng;
+use rand::prelude::IndexedRandom;
+use std::ops::Index;
+use neural_network::{NeuralNetwork, LayerTopology};
+use std::ops::{Deref, DerefMut};
+
+
+
+pub const TIME_STEP: f32 = 0.1;
+pub const BASE_FITNESS: usize = 5;
+pub const MUTATION_RATE: f32 = 0.1;
+pub const MUTATION_AMOUNT: f32 = 0.5;
+
+#[derive(Debug)]
+pub struct Animal {
+    pub id: usize,
+    pub pos: (f32, f32),
+    pub sight: Vec<f32>,
+    pub speed: f32,
+    pub angle: f32,
+    pub fitness: usize,
+    pub brain: NeuralNetwork,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Animal {
+    pub fn new(id: usize, pos: (f32, f32), sight: Vec<f32>, brain_topology: &[LayerTopology]) -> Self {
+        
+        let brain = NeuralNetwork::new(brain_topology);
+        let out = brain.propagate(sight.clone());
+        let speed = out[0];
+        let angle = out[1];
+        Self { id, pos, sight, speed, angle, fitness: BASE_FITNESS, brain }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn update_sight(&mut self, new_sight: Vec<f32>) {
+        self.sight = new_sight;
+        let out = self.brain.propagate(new_sight);
+        self.speed = out[0];
+        self.angle = out[1];
+    }
+
+    pub fn step(&mut self) {
+        self.pos.0 += self.speed * TIME_STEP * self.angle.cos();
+        self.pos.1 += self.speed * TIME_STEP * self.angle.sin();
+    }
+
+    pub fn mutate(&mut self) {
+        self.brain.mutate(MUTATION_RATE, MUTATION_AMOUNT);
+    }
+
+}
+
+impl Clone for Animal {
+    fn clone(&self) -> Self {
+        Self {
+            id: self.id,
+            pos: self.pos,
+            sight: self.sight.clone(),
+            speed: self.speed,
+            angle: self.angle,
+            fitness: self.fitness,
+            brain: self.brain.clone(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Prey(Animal);
+pub struct Predator(Animal);
+
+
+
+impl Deref for Prey {
+    type Target = Animal;
+
+    fn deref(&self) -> &Self::Target {
+        &self.animal
+    }
+}
+
+impl DerefMut for Prey {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.animal
+    }
+}
+
+impl Deref for Predator {
+    type Target = Animal;
+
+    fn deref(&self) -> &Self::Target {
+        &self.animal
+    }
+}
+
+impl DerefMut for Predator {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.animal
+    }
+}
+
+
+impl Prey {
+    pub fn new(id: usize, pos: (f32, f32), sight: Vec<f32>, brain_topology: &[LayerTopology]) -> Self {
+        let animal = Animal::new(id, pos, sight, brain_topology);
+        Self(animal)
+    }
+}
+
+
+impl Predator {
+    pub fn new(id: usize, pos: (f32, f32), sight: Vec<f32>, brain_topology: &[LayerTopology]) -> Self {
+        let animal = Animal::new(id, pos, sight, brain_topology);
+        Self(animal)
     }
 }
--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,14 +1,152 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/*
+A simple feedforward implementation of a neural network.
+Each neuron uses the ReLU activation function (can be changed later).
+Given some Layer-Topology, the network is constructed with random weights and biases and fully connected.
+*/
+
+
+use rand::Rng;
+
+
+
+
+#[derive(Debug)]
+pub struct NeuralNetwork {
+    layers: Vec<Layer>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+#[derive(Debug)]
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
+
+impl Layer {
+    fn new(input_size: usize, output_size: usize) -> Self {
+        let mut neurons = Vec::with_capacity(output_size);
+        for _ in 0..output_size {
+            neurons.push(Neuron::new(input_size));
+        }
+        Self { neurons }
     }
+
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        let mut outputs = Vec::new();
+        for neuron in &self.neurons {
+            let output = neuron.propagate(&inputs);
+            outputs.push(output);
+        }
+        outputs
+    }
+}
+
+
+impl Neuron {
+    fn new(input_size: usize) -> Self {
+        let mut rng = rand::rng();
+        let bias = rng.random_range(-1.0..=1.0);
+        let weights = (0..input_size).map(|_| rng.random_range(-1.0..=1.0)).collect();
+        Self { bias, weights }
+    }
+    
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let mut output = 0.0;
+
+        for i in 0..inputs.len() {
+            output += inputs[i] * self.weights[i];
+        }
+
+        output += self.bias;
+
+        if output > 0.0 {
+            output} else {
+                0.0
+            }
+        }
+}
+
+
+
+impl NeuralNetwork {
+    pub fn new(layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1, "Network must have at least input and output layers");
+        let mut network_layers = Vec::new();
+
+        for i in 0..layers.len() - 1 {
+            let input_size = layers[i].neurons;
+            let output_size = layers[i + 1].neurons;
+
+            network_layers.push(Layer::new(input_size, output_size));
+            
+        }
+
+        Self { layers: network_layers }
+    }
+
+
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        for layer in &self.layers {
+            inputs = layer.propagate(inputs);
+        }
+        inputs
+    }
+
+    pub fn mutate(&mut self, mutation_rate: f32, mutation_amount: f32) {
+        let mut rng = rand::rng();
+
+        for layer in &mut self.layers {
+            for neuron in &mut layer.neurons {
+                if rng.random_float() < mutation_rate {
+                    neuron.bias += rng.random_range(-mutation_amount..=mutation_amount);
+                }
+                for weight in &mut neuron.weights {
+                    if rng.random_float() < mutation_rate {
+                        *weight += rng.random_range(-mutation_amount..=mutation_amount);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn print_structure(&self) {
+        for (i, layer) in self.layers.iter().enumerate() {
+            println!("Layer {}: {} neurons", i, layer.neurons.len());
+            for (j, neuron) in layer.neurons.iter().enumerate() {
+                println!("  Neuron {}: bias = {}, weights = {:?}", j, neuron.bias, neuron.weights);
+            }
+        }
+    }
+
+    pub fn clone(&self) -> Self {
+        let mut new_layers = Vec::new();
+
+        for layer in &self.layers {
+            let mut new_neurons = Vec::new();
+            for neuron in &layer.neurons {
+                let new_neuron = Neuron {
+                    bias: neuron.bias,
+                    weights: neuron.weights.clone(),
+                };
+                new_neurons.push(new_neuron);
+            }
+            let new_layer = Layer { neurons: new_neurons };
+            new_layers.push(new_layer);
+        }
+
+        Self { layers: new_layers }
+    }
+
 }
