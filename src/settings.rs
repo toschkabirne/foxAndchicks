5545d81fb9d494f878f236f94ae58d480c7395e1
Anton Herzberg <herzberg097@gmail.com>
2026-02-02 21:33:44 +0100

    bug fix with parm_search, mainly just about that and related

--- src/settings.rs
+++ src/settings.rs
@@ -21,9 +21,9 @@ use std::sync::RwLock;
 // ----------------------------------------
 
 // Mutation / Bias params, only change the numbers, not the variables
-static ADD_NEURON: RwLock<f32> = RwLock::new(0.15);
-static ADD_WEIGHT: RwLock<f32> = RwLock::new(0.60);
-static CHANGE_WEIGHT: RwLock<f32> = RwLock::new(0.60);
+static ADD_NEURON: RwLock<f32> = RwLock::new(0.14);
+static ADD_WEIGHT: RwLock<f32> = RwLock::new(0.5);
+static CHANGE_WEIGHT: RwLock<f32> = RwLock::new(0.80);
 static BIAS: RwLock<f32> = RwLock::new(0.5);
 
 // This is the step size applied when an weight is changed
